2e8ba5d329d75a89212bca40c9b83a5995fcc802
SHREEJ <SHREEJ@LAPTOP-44E61TJ5>
2020-09-04 23:58:12 +0530

    commited by shreej

--- abc/src/abc/aa.java
+++ abc/src/abc/aa.java
@@ -6,12 +6,12 @@ public class aa {
 	public static void main(String[] args) {
 
 	Scanner sc=new Scanner(System.in);
-	System.out.println("Enter string");
+	System.out.println("Enter Word here=");
 	String ip=sc.next();
 	int length=ip.length();
 	int middle=(length/2);
 	char ch =ip.charAt(middle);
-	System.out.println("ch");
+	System.out.println("Middle Character is="+ch);
 	
 }
 }
